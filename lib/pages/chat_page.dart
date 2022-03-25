56d3a80c451be92e9f051911ce03c74132f07b2e
ahmadm712 <ahmadmuji2808@gmail.com>
2022-03-25 15:58:12 +0700

    semua fitur sudah bisa semua

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,19 +1,55 @@
 import 'package:chatting_app/pages/login_page.dart';
+import 'package:chatting_app/widgets/message_bubble.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
-class ChatPage extends StatelessWidget {
+class ChatPage extends StatefulWidget {
   static const String id = 'chat_page';
+
+  @override
+  State<ChatPage> createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  late User _activeUser;
   var _auth = FirebaseAuth.instance;
+  var _firestore = FirebaseFirestore.instance;
+  final _messageTextController = TextEditingController();
+  void getCurrentUser() async {
+    try {
+      var currentUser = await _auth.currentUser;
+
+      if (currentUser != null) {
+        _activeUser = currentUser;
+      }
+    } catch (e) {
+      print(e);
+    }
+  }
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getCurrentUser();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _messageTextController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Chat Room'),
+        title: const Text('Chat Room'),
         actions: [
           IconButton(
-            icon: Icon(Icons.close),
+            icon: const Icon(Icons.close),
             tooltip: 'Logout',
             onPressed: () async {
               await _auth.signOut();
@@ -27,26 +63,62 @@ class ChatPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: Placeholder(),
+              child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+                stream: _firestore
+                    .collection('messages')
+                    .orderBy('dateCreated', descending: true)
+                    .snapshots(),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                  return ListView(
+                    reverse: true,
+                    padding: const EdgeInsets.symmetric(
+                      horizontal: 8.0,
+                      vertical: 16.0,
+                    ),
+                    children: snapshot.data!.docs.map((document) {
+                      final messageText = document.data()['text'];
+                      final messageSender = document.data()['sender'];
+                      return MessageBubble(
+                        sender: messageSender,
+                        text: messageText,
+                        isMyChat: messageSender == _activeUser.email,
+                      );
+                    }).toList(),
+                  );
+                },
+              ),
             ),
-            SizedBox(height: 8),
+            const SizedBox(height: 8),
             Row(
               children: [
                 Expanded(
                   child: TextField(
-                    decoration: InputDecoration(
+                    controller: _messageTextController,
+                    decoration: const InputDecoration(
                       contentPadding:
                           EdgeInsets.symmetric(vertical: 8.0, horizontal: 16.0),
                       border: OutlineInputBorder(),
                     ),
                   ),
                 ),
-                SizedBox(width: 8),
+                const SizedBox(width: 8),
                 MaterialButton(
-                  child: Text('SEND'),
+                  child: const Text('SEND'),
                   color: Theme.of(context).primaryColor,
                   textTheme: ButtonTextTheme.primary,
-                  onPressed: () {},
+                  onPressed: () {
+                    _firestore.collection('messages').add({
+                      'text': _messageTextController.text,
+                      'sender': _activeUser.email,
+                      'dateCreated': Timestamp.now()
+                    });
+                    _messageTextController.clear();
+                  },
                 ),
               ],
             ),
